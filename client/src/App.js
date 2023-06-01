487a42d2ebeb1c7ea0f7a9cb74f9ff531df16727
Ambeeru Vignesh <vigneshvj53@gmail.com>
2023-06-01 20:52:57 +0530

    authentication frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { accessToken, logout } from "./Spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get("access_token");
-    const refreshToken = urlParams.get("refresh_token");
-
-    console.log(accessToken);
-    console.log(refreshToken);
-
-    if (refreshToken) {
-      fetch(`/request_token?refresh_token=${refreshToken}`)
-        .then((response) => response.json)
-        .then((data) => console.log(data))
-        .catch((err) => console.error(err));
-    }
+    setToken(accessToken);
+    console.log(token);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="http://localhost:5000/login"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          Login to Spotify
-        </a>
+        {!token ? (
+          <a
+            className="App-link"
+            href="http://localhost:5000/login"
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Logged In!</h1>
+            <button onClick={logout}>Logout</button>
+          </>
+        )}
       </header>
     </div>
   );

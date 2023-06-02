62abee79aee05f2e0059095164bc5bcb8772f56e
Ambeeru Vignesh <vigneshvj53@gmail.com>
2023-06-02 16:40:17 +0530

    styles css

--- client/src/Spotify.js
+++ client/src/Spotify.js
@@ -107,3 +107,29 @@ const getAccessToken = () => {
 };
 
 export const accessToken = getAccessToken();
+
+axios.defaults.baseURL = "https://api.spotify.com/v1";
+axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
+axios.defaults.headers["Content-Type"] = "application/json";
+
+export const getCurrentUserProfile = () => axios.get("/me");
+
+export const getCurrentUserPlaylists = (limit = 20) => {
+  return axios.get(`/me/playlists?limit=${limit}`);
+};
+
+export const getTopArtists = (time_range = "short_term") => {
+  return axios.get(`/me/top/artists?time_range=${time_range}`);
+};
+
+export const getTopTracks = (time_range = "short_term") => {
+  return axios.get(`/me/top/tracks?time_range=${time_range}`);
+};
+
+export const getPlaylistById = (playlist_id) => {
+  return axios.get(`/playlists/${playlist_id}`);
+};
+
+export const getAudioFeaturesForTracks = (ids) => {
+  return axios.get(`/audio-features/${ids}`);
+};
